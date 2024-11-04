916aaf252ebdcb9d07814e6bd0f7ec08d66dd3e1
Vinicius Henrique <08vinic@gmail.com>
2024-11-04 01:08:14 -0300

    cards de funcionalidades

--- demo-web/src/App.js
+++ demo-web/src/App.js
@@ -9,6 +9,7 @@ import SideBar from "./components/template/SideBar";
 import Footer from "./components/template/Footer";
 import {UserProvider} from "./store/UsuarioContext";
 import CondominioDetailsPage from './pages/CondominioDetailsPage';
+import CardItem from "./components/template/CardItem";
 
 const App = () => {
     return (
@@ -19,7 +20,37 @@ const App = () => {
                 <main className="main-content position-relative border-radius-lg ">
                     <NavBar/>
                     <div className="container-fluid py-4">
+                        <div className="row">
+                            <div className="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                                <CardItem
+                                    title="Reservar um espaço"
+                                    icon="ni ni-money-coins text-lg opacity-10"
+                                    iconContainerClass="bg-gradient-success shadow-success" // Exemplo de classe personalizada
+                                />
+                            </div>
+                            <div className="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                                <CardItem
+                                    title="Cadastrar morador"
+                                    icon="ni ni-world text-lg opacity-10"
+                                    iconContainerClass="bg-gradient-danger shadow-danger" // Exemplo de classe personalizada
+                                />
+                            </div>
+                            <div className="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                                <CardItem
+                                    title="Cadastrar encomenda"
+                                    icon="ni ni-paper-diploma text-lg opacity-10"
+                                    iconContainerClass="bg-gradient-success shadow-success" // Exemplo de classe personalizada
+                                />
+                            </div>
+                            <div className="col-xl-3 col-sm-6">
+                                <CardItem
+                                    title="Alugueis de apartamento"
+                                    icon="ni ni-cart text-lg opacity-10"
+                                    iconContainerClass="bg-gradient-warning shadow-warning" // Exemplo de classe personalizada
+                                />
+                            </div>
 
+                        </div>
                         <Routes>
                             <Route path="/roles" element={<RolesPage/>}/>
                             <Route path="/usuarios" element={<UsuariosPage/>}/>
