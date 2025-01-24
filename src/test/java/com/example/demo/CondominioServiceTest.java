950e2903a29b4babafe446138eb7eb410aec684c
Vinicius Henrique <vinicius.snts08@gmail.com>
2025-01-24 02:47:52 -0300

    banco certo

--- src/test/java/com/example/demo/CondominioServiceTest.java
+++ src/test/java/com/example/demo/CondominioServiceTest.java
@@ -8,10 +8,12 @@ import com.example.demo.repository.UsuarioRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class CondominioServiceTest {
 
     @Autowired
