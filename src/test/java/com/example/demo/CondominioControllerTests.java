e4a2a36c2753e2e99d44f93fac29d4c679d65d69
João Paulo Jerônimo <joaopaulojeronimo00@gmail.com>
2025-01-24 01:22:23 -0300

    feat: adiciona testes

---
+++ src/test/java/com/example/demo/CondominioControllerTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class CondominioControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnListOfCondominiums() throws Exception {
+        mockMvc.perform(get("/api/condominiums"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").isNotEmpty());
+    }
+}
